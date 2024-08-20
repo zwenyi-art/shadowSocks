17724fc9a50b15b5d37499d67d5daab8c4807595
Zwe Nyi Nyi Htwe <zwenyinyihtwe@hotmail.com>
2024-08-20 15:10:28 +0630

    sResponse modified

--- shadowSocksTraffic/sResponse.js
+++ shadowSocksTraffic/sResponse.js
@@ -5,7 +5,6 @@ app.get("/usage", (req, res) => {
   const shadowsocksURL =
     "ss://YWVzLTI1Ni1nY206cGFzc3dvcmQxMjM@206.206.77.119:8388#Testing%20";
   const filename = "shadowsocks-url.txt";
-
   // Set headers to indicate it's a text file and force download
   res.setHeader("Content-Disposition", `attachment; filename="${filename}"`);
   res.setHeader("Content-Type", "text/plain");
@@ -14,6 +13,14 @@ app.get("/usage", (req, res) => {
   res.send(shadowsocksURL);
 });
 
+app.get("/client", (req, res) => {
+  const shadowsocksURL =
+    "ss://YWVzLTI1Ni1nY206cGFzc3dvcmQxMjM@206.206.77.119:8388#Testing%20";
+
+  res.json({
+    shadowsocksURL: shadowsocksURL,
+  });
+});
 app.listen(5000, () => {
   console.log("Express server running on port 5000");
 });
