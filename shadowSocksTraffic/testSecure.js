25f960c12840345f356806cbd165a0f1c9a8634b
Zwe Nyi Nyi Htwe <zwenyinyihtwe@hotmail.com>
2024-08-20 19:18:31 +0630

    respone ss txt file

--- shadowSocksTraffic/testSecure.js
+++ shadowSocksTraffic/testSecure.js
@@ -13,7 +13,17 @@ const sslOptions = {
 app.get("/", (req, res) => {
   res.send("Hello, your connection is secure!");
 });
+app.get("/ss", (req, res) => {
+  const shadowsocksURL =
+    "ss://YWVzLTI1Ni1nY206cGFzc3dvcmQxMjM@206.206.77.119:8388#Testing%20";
+  const filename = "shadowsocks-url.txt";
+  // Set headers to indicate it's a text file and force download
+  res.setHeader("Content-Disposition", `attachment; filename="${filename}"`);
+  res.setHeader("Content-Type", "text/plain");
 
+  // Send the Shadowsocks URL as the file content
+  res.send(shadowsocksURL);
+});
 // Create an HTTPS server with the SSL options
 https.createServer(sslOptions, app).listen(443, () => {
   console.log("HTTPS Express server running on port 443");
